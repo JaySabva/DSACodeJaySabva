794c8dd6bddf0203cb5059a587d1ca67063d6365
Jay Sabva <94957904+JaySabva@users.noreply.github.com>
2022-05-25 16:16:52 +0530

    Update and rename Insertion to Insertion.cpp

---
+++ DSA/Array/Insertion.cpp
@@ -0,0 +1,36 @@
+//Author-SABVA JAY DILIPBHAI
+#include<iostream>
+using namespace std;
+int main()
+{
+    int size,x,pos;
+    cout<<"Enter the size of array: ";
+    cin>>size;
+
+    int arr[size];
+    for(int i=0;i<size;i++)
+    {
+        cout<<"Enter the element: ";
+        cin>>arr[i];
+    }
+    cout<<"Enter the element to be inserted: ";
+    cin>>x;
+    cout<<"Enter the position: ";
+    cin>>pos;
+    if(pos > size)
+    {
+        cout<<"Invalid position"<<endl;
+        return 0;
+    }
+    for(int i=size-1;i>=pos;i--)//Ek Ek Element Next Side Transfer Karvanu
+    {
+        arr[i+1] = arr[i];
+    }
+    arr[pos] = x;
+    cout<<"The array after insertion is: ";
+    for(int i=0;i<size+1;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
