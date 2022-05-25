7b585cae2890989b3f82c4955ee1f8e34b2ec55f
Jay Sabva <94957904+JaySabva@users.noreply.github.com>
2022-05-25 21:19:45 +0530

    Create CircularQueue.cpp

---
+++ DSA/Queue/CircularQueue.cpp
@@ -0,0 +1,117 @@
+// Circular Queue implementation in C++
+
+#include <iostream>
+#define SIZE 5 /* Size of Circular Queue */
+
+using namespace std;
+
+class Queue {
+   private:
+  int items[SIZE], front, rear;
+
+   public:
+  Queue() {
+    front = -1;
+    rear = -1;
+  }
+  // Check if the queue is full
+  bool isFull() {
+    if (front == 0 && rear == SIZE - 1) {
+      return true;
+    }
+    if (front == rear + 1) {
+      return true;
+    }
+    return false;
+  }
+  // Check if the queue is empty
+  bool isEmpty() {
+    if (front == -1)
+      return true;
+    else
+      return false;
+  }
+  // Adding an element
+  void enQueue(int element) {
+    if (isFull()) {
+      cout << "Queue is full";
+    } else {
+      if (front == -1) front = 0;
+      rear = (rear + 1) % SIZE;
+      items[rear] = element;
+      cout << endl
+         << "Inserted " << element << endl;
+    }
+  }
+  // Removing an element
+  int deQueue() {
+    int element;
+    if (isEmpty()) {
+      cout << "Queue is empty" << endl;
+      return (-1);
+    } else {
+      element = items[front];
+      if (front == rear) {
+        front = -1;
+        rear = -1;
+      }
+      // Q has only one element,
+      // so we reset the queue after deleting it.
+      else {
+        front = (front + 1) % SIZE;
+      }
+      return (element);
+    }
+  }
+
+  void display() {
+    // Function to display status of Circular Queue
+    int i;
+    if (isEmpty()) {
+      cout << endl
+         << "Empty Queue" << endl;
+    } else {
+      cout << "Front -> " << front;
+      cout << endl
+         << "Items -> ";
+      for (i = front; i != rear; i = (i + 1) % SIZE)
+        cout << items[i];
+      cout << items[i];
+      cout << endl
+         << "Rear -> " << rear;
+    }
+  }
+};
+
+int main() {
+  Queue q;
+  int choice;
+  do {
+    cout << "1. Insert an element" << endl;
+    cout << "2. Delete an element" << endl;
+    cout << "3. Display status of queue" << endl;
+    cout << "4. Exit" << endl;
+    cout << "Enter your choice: ";
+    cin >> choice;
+    switch (choice) {
+      case 1:
+        int element;
+        cout << "Enter element to be inserted: ";
+        cin >> element;
+        q.enQueue(element);
+        break;
+      case 2:
+        q.deQueue();
+        break;
+      case 3:
+        q.display();
+        break;
+      case 4:
+        break;
+      default:
+        cout << "Wrong choice" << endl;
+    }
+  } while (choice != 4);
+  
+  return 0;
+}
